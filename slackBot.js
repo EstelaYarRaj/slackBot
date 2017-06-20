316068b2983a1703a6c79f7c6fcc9d8203a7c766
EstelaYarRaj <madichrajson@gmail.com>
2017-06-20 12:01:51 +0800

    'ambience to ambient' and remove extra 'mention'

--- slackBot.js
+++ slackBot.js
@@ -9,7 +9,7 @@ bot.startRTM(function(err,bot,payload){
 	if(err){
 		throw new Error('Failure to connect')
 	}
-	botCntrl.hears(["Hello","Hi","Good day"], ["direct_message","direct_mention","mention","ambience","mention"], function(bot,message){
+	botCntrl.hears(["Hello","Hi","Good day"], ["direct_message","direct_mention","mention","ambient"], function(bot,message){
 		bot.reply(message, "Hello there my name is SlackBot123.")
 	})
 })
