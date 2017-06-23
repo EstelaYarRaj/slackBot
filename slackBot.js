9eb6a5f01d36eb9f652e83bf76ecf6faa05e814f
EstelaYarRaj <madichrajson@gmail.com>
2017-06-23 14:16:13 +0800

    npm prompt added

--- slackBot.js
+++ slackBot.js
@@ -1,21 +1,20 @@
 var botkit = require('botkit');
 var botCntrl = botkit.slackbot();
+var prompt = require('prompt');
 
-System.out.println("Enter token: ");
-
-Scanner reader = new Scanner(System.in);
-string userToken = reader.nextString();
-reader.close();
-
-var bot = botCntrl.spawn({
-	token : userToken;
-})
-
-bot.startRTM(function(err,bot,payload){
-	if(err){
-		throw new Error('Failure to connect')
-	}
-	botCntrl.hears(["Hello","Hi","Good day"], ["direct_message","direct_mention","mention","ambient"], function(bot,message){
-		bot.reply(message, "Hello there my name is SlackBot123.")
+prompt.get(['token'], function(err, result){
+	console.log('Enter Token :' + result.token);
+	var bot = botCntrl.spawn({
+		token : result.token 
 	})
-})
+
+	bot.startRTM(function(err,bot,payload){
+		if(err){
+			throw new Error('Failure to connect')
+		}
+		botCntrl.hears(["Hello","Hi","Good day"], ["direct_message","direct_mention","mention","ambient"], function(bot,message){
+			bot.reply(message, "Hello there my name is SlackBot123.")
+		})
+	})
+});
+
