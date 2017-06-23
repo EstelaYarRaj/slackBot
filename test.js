e993fb8f0f8438a485376459aeeee27560997f2d
EstelaYarRaj <madichrajson@gmail.com>
2017-06-23 15:23:48 +0800

    test file

---
+++ test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../slackbot.js');
+
+describe('GET/', function(){
+	it('responds with Attempting to connect to Slack...', function(done){
+		request(app).get('/').expect('Attempting to connect to Slack...', done);
+	})
+})
